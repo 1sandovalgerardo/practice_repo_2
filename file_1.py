279073a7e06e06687eaeb9a3e0511629e0bf0dee
1sandovalgerardo <1sandovalgerardo@gmail.com>
2020-12-13 00:04:08 -0600

    added new print statement

--- file_1.py
+++ file_1.py
@@ -5,3 +5,5 @@ print('second message now the repo exists')
 print('this message should appear in branch_1 branch if I pull correctly')
 
 print('second test of pull steps')
+
+print('message from branch 1')
